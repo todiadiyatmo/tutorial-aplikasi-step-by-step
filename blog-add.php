98e53ab7cc9110cc3e3aeb9a92b1c37d37a9a665
Yayan <heri.sulistyo@ai.astra.co.id>
2024-05-02 11:23:13 +0700

    membuat tampilan menu sederhana

---
+++ blog-add.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Posts</title>
+    <!-- Include Bootstrap CSS from a CDN -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<?php include "blog-nav.php" ?>
+
+</body>
+
+</html>
+
+<?php
+// Close the database connection
+$conn->close();
